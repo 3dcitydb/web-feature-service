0246f2a8f9a8d2e2beff9a5c30bc4a3f5ad7f134
Claus Nagel <cnagel@virtualcitysystems.de>
2018-02-27 13:33:03 +0100

    improved lazy database connector to not block the service

--- src/vcs/citydb/wfs/util/DatabaseConnector.java
+++ src/vcs/citydb/wfs/util/DatabaseConnector.java
@@ -29,55 +29,61 @@ public class DatabaseConnector {
 		if (connectionPool.isConnected())
 			return;
 
-		lock.lock();
-		try {
-			if (connectionPool.isConnected())
-                return;
-			
-			Logger log = Logger.getInstance();
-			Database databaseConfig = exporterConfig.getProject().getDatabase();
-			if (databaseConfig.getActiveConnection() == null) {
-				WFSExceptionMessage message = new WFSExceptionMessage(WFSExceptionCode.INTERNAL_SERVER_ERROR);
-                message.addExceptionText("Failed to connect to the database.");
-                message.addExceptionText("No database connection provided in " + Constants.CONFIG_PATH + '/' + Constants.CONFIG_FILE + '.');
-                throw new WFSException(message);
-			}
-
-			DBConnection connection = databaseConfig.getActiveConnection();
-			connection.setInternalPassword(connection.getPassword());		
-
+		if (lock.tryLock()) {
 			try {
-				connectionPool.connect(exporterConfig);
-			} catch (DatabaseConfigurationException | SQLException e) {
-				throw new WFSException(WFSExceptionCode.INTERNAL_SERVER_ERROR, "Failed to connect to the database.", e);
-			} catch (DatabaseVersionException e) {
-				WFSExceptionMessage message = new WFSExceptionMessage(WFSExceptionCode.INTERNAL_SERVER_ERROR);
-                message.addExceptionText("Failed to connect to the database.");
-                message.addExceptionText(e.getMessage());
-                message.addExceptionText("Supported versions are '" + Util.collection2string(e.getSupportedVersions(), ", ") + "'.");
-                throw new WFSException(message);
-			}
+				if (connectionPool.isConnected())
+					return;
 
-			log.info("Database connection established.");
-			AbstractDatabaseAdapter adapter = connectionPool.getActiveDatabaseAdapter();
-			adapter.getConnectionMetaData().printToConsole();
+				Logger log = Logger.getInstance();
+				Database databaseConfig = exporterConfig.getProject().getDatabase();
+				if (databaseConfig.getActiveConnection() == null) {
+					WFSExceptionMessage message = new WFSExceptionMessage(WFSExceptionCode.INTERNAL_SERVER_ERROR);
+					message.addExceptionText("Failed to connect to the database.");
+					message.addExceptionText("No database connection provided in " + Constants.CONFIG_PATH + '/' + Constants.CONFIG_FILE + '.');
+					throw new WFSException(message);
+				}
 
-			// log whether user-defined SRSs are supported
-			for (DatabaseSrs refSys : exporterConfig.getProject().getDatabase().getReferenceSystems()) {
-				if (refSys.isSupported())
-					log.info("Reference system '" + refSys.getDescription() + "' (SRID: " + refSys.getSrid() + ") supported.");
-				else
-					log.warn("Reference system '" + refSys.getDescription() + "' (SRID: " + refSys.getSrid() + ") NOT supported.");
+				DBConnection connection = databaseConfig.getActiveConnection();
+				connection.setInternalPassword(connection.getPassword());		
+
+				try {
+					connectionPool.connect(exporterConfig);
+				} catch (DatabaseConfigurationException | SQLException e) {
+					throw new WFSException(WFSExceptionCode.INTERNAL_SERVER_ERROR, "Failed to connect to the database.", e);
+				} catch (DatabaseVersionException e) {
+					WFSExceptionMessage message = new WFSExceptionMessage(WFSExceptionCode.INTERNAL_SERVER_ERROR);
+					message.addExceptionText("Failed to connect to the database.");
+					message.addExceptionText(e.getMessage());
+					message.addExceptionText("Supported versions are '" + Util.collection2string(e.getSupportedVersions(), ", ") + "'.");
+					throw new WFSException(message);
+				}
+
+				log.info("Database connection established.");
+				AbstractDatabaseAdapter adapter = connectionPool.getActiveDatabaseAdapter();
+				adapter.getConnectionMetaData().printToConsole();
+
+				// log whether user-defined SRSs are supported
+				for (DatabaseSrs refSys : exporterConfig.getProject().getDatabase().getReferenceSystems()) {
+					if (refSys.isSupported())
+						log.info("Reference system '" + refSys.getDescription() + "' (SRID: " + refSys.getSrid() + ") supported.");
+					else
+						log.warn("Reference system '" + refSys.getDescription() + "' (SRID: " + refSys.getSrid() + ") NOT supported.");
+				}
+
+				// log connection warnings
+				List<DatabaseConnectionWarning> warnings = adapter.getConnectionWarnings();
+				if (!warnings.isEmpty()) {
+					for (DatabaseConnectionWarning warning : warnings)
+						log.warn(warning.getMessage());
+				}
+			} finally {
+				lock.unlock();
 			}
-			
-			// log connection warnings
-            List<DatabaseConnectionWarning> warnings = adapter.getConnectionWarnings();
-            if (!warnings.isEmpty()) {
-                for (DatabaseConnectionWarning warning : warnings)
-                    log.warn(warning.getMessage());
-            }
-		} finally {
-			lock.unlock();
+		} else {
+			WFSExceptionMessage message = new WFSExceptionMessage(WFSExceptionCode.SERVICE_UNAVAILABLE);
+			message.addExceptionText("The service is currently trying to connect to the database.");
+			message.addExceptionText("This is a temporary state. Please retry later.");
+			throw new WFSException(message);
 		}
 	}
 
